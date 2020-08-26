9a9317dcb086e606557a1be0cfa96be4c736e21f
rasulovVI <kingrasulov@mail.ru>
2020-08-26 16:50:20 +0500

    initial

---
+++ MyPlaces/CellTable.swift
@@ -0,0 +1,18 @@
+//
+//  CellTable.swift
+//  MyPlaces
+//
+//  Created by Orienbank on 26.08.2020.
+//  Copyright © 2020 Orienbank. All rights reserved.
+//
+
+import UIKit
+
+class CellTable: UITableViewCell {
+
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+}
